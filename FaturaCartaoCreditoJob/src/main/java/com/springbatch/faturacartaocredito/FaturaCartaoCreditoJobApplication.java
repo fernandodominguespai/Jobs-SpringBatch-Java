c55e9d939daff33a3c376926b1415538cc1ffdd6
Fernando Domingues <62526692+fernandodominguespai@users.noreply.github.com>
2025-05-19 11:38:40 -0300

    Add files via upload

---
+++ FaturaCartaoCreditoJob/src/main/java/com/springbatch/faturacartaocredito/FaturaCartaoCreditoJobApplication.java
@@ -0,0 +1,15 @@
+package com.springbatch.faturacartaocredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaturaCartaoCreditoJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaturaCartaoCreditoJobApplication.class, args);
+//		ConfigurableApplicationContext context = SpringApplication.run(FaturaCartaoCreditoJobApplication.class, args);
+//		context.close();
+	}
+
+}
